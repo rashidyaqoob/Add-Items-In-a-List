d14480cc7909fe6248adebee3aa1271ba7a665c4
rashidyaqoob <53550890+rashidyaqoob@users.noreply.github.com>
2019-11-20 22:07:12 +0530

    update constants

--- addlist.js
+++ addlist.js
@@ -1,3 +1,6 @@
+const addItems=document.querySelector(".add-items");
+const itemsList=document.querySelector(".plates");
+const items=[];
 function addItem(e){
 	e.preventDefault();
 	const text=(this.querySelector("[name=item]")).value;
